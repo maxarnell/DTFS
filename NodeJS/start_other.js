57b7ae6c4e3277c089051093f82ac348ded59825
maxarnell <arnell@mit.edu>
2018-05-21 16:40:40 -0400

    added part of intake

---
+++ NodeJS/start_other.js
@@ -0,0 +1,5 @@
+start other
+
+module = 1384719
+
+Hi, I’m the MIT ChatBot. Text CONSENT to answer a few questions. More info at http://equitytransit.info or contact Pierre at (617)453-8285.
---
+++ NodeJS/welcome.js
@@ -0,0 +1,5 @@
+Welcome
+
+module = 1374920
+
+Hi! I’m the MIT ChatBot. For English reply 1,  Espanol respuesta 2,  Haitian Creole-3, Cape Verdean Creole-4, Chinese-5, Vietnamese-6.
